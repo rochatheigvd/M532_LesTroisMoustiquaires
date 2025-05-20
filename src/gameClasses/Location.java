c2e12f7c19970fb7daa18b3c2221ac4b6bcddecd
rochatheigvd <yannis.rochat@hes-so.ch>
2025-05-20 11:20:52 +0200

    Player & Location methods

--- src/gameClasses/Location.java
+++ src/gameClasses/Location.java
@@ -8,4 +8,79 @@ public class Location {
     private boolean isLocked;
     private ArrayList<Puzzle> puzzleList;
     private ArrayList<Item> itemList;
+
+    public Location(String name, String descr, boolean isLocked) {
+        this.descr = descr;
+        this.isLocked = isLocked;
+        this.name = name;
+        this.puzzleList = new ArrayList<Puzzle>();
+        this.itemList = new ArrayList<Item>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescr() {
+        return this.descr;
+    }
+
+    public boolean getIsLocked() {
+        return this.isLocked;
+    }
+
+    public ArrayList<Puzzle> getPuzzleList() {
+        return puzzleList;
+    }
+
+    public ArrayList<Item> getItemList() {
+        return itemList;
+    }
+
+    public boolean dropPuzzle(Puzzle puzzle) {
+        if (this.puzzleList.contains(puzzle)) {
+            this.puzzleList.remove(puzzle);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean dropItem(Item item) {
+        if (this.itemList.contains(item)) {
+            this.itemList.remove(item);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean unlockLocation() {
+
+        if (this.isLocked == false) {
+            this.isLocked = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addItemToList(Item item) {
+        if (this.itemList.contains(item)) {
+            return false;
+        } else {
+            this.itemList.add(item);
+            return true;
+        }
+    }
+
+    public boolean addPuzzleToList(Puzzle puzzle) {
+        if (this.puzzleList.contains(puzzle)) {
+            return false;
+        } else {
+            this.puzzleList.add(puzzle);
+            return true;
+        }
+    }
+
 }
--- src/gameClasses/Player.java
+++ src/gameClasses/Player.java
@@ -3,4 +3,25 @@ package gameClasses;
 public class Player {
     private int[] playerPosition;
     private Inventory inventory;
+
+    public Player() {
+        this.playerPosition = new int[2];
+        this.inventory = null;
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public void setPlayerPosition(int[] position) {
+        this.playerPosition = position;
+    }
+
+    public int[] getPlayerPosition() {
+        return this.playerPosition;
+    }
 }
