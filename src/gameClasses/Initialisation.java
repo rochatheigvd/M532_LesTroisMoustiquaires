ee65dc94ec19baa02702f73032d442ef90e83f4f
Keedjud <inoewenger@gmail.com>
2025-05-20 12:06:16 +0200

    Update Initialisation.java

--- src/gameClasses/Initialisation.java
+++ src/gameClasses/Initialisation.java
@@ -44,8 +44,7 @@ public class Initialisation {
     }
 
     private void createInventory() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createInventory'");
+        Inventory inventory = new Inventory();
     }
 
     private void createPlayer() {
