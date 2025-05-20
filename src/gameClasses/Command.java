230f7b8599655b90cac96e4caa483ef04f4b8105
Unseile <lea.haeberli03@gmail.com>
2025-05-20 11:17:50 +0200

    Attribut/méthodes

--- src/gameClasses/Command.java
+++ src/gameClasses/Command.java
@@ -8,3 +8,32 @@ public abstract class Command implements ICommand {
     private String verb;
     private static Game game;
 }
+
+    public Command(String verb, String description) {
+        this.verb = verb;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVerb() {
+        return verb;
+    }
+
+    public static Game getGame() {
+        return game;
+    }
+
+    public static void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Location getPlayerLocation() {
+        return Location;
+    }
+
+    public abstract void execute(String[] args);
+
+}
