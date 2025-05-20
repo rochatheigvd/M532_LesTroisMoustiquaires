4f2408a0dc2f79d3ae3b34597bd6637a1c87aafa
Unseile <lea.haeberli03@gmail.com>
2025-05-20 11:58:48 +0200

    inventory/item/key/letter

--- src/gameClasses/Inventory.java
+++ src/gameClasses/Inventory.java
@@ -4,4 +4,24 @@ import java.util.ArrayList;
 
 public class Inventory {
     private ArrayList<Item> itemList;
+
+    public Inventory() {
+        this.itemList = new ArrayList<>();
+    }
+
+    getItemList() {
+        return this.itemList;
+    }
+
+    public void addItem(Item item) {
+        this.itemList.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.itemList.remove(item);
+    }
+
+    public Item getItem(String name) { 
+           
+    }
 }
--- src/gameClasses/Item.java
+++ src/gameClasses/Item.java
@@ -6,4 +6,26 @@ public abstract class Item implements IInspectable {
     private String name;
     private String lookDescr;
     private String inspectDescr;
+
+    public Item(String name, String lookDescr, String inspectDescr) {
+        this.name = name;
+        this.lookDescr = lookDescr;
+        this.inspectDescr = inspectDescr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLookDescr() {
+        return lookDescr;
+    }
+
+    public String getInspectDescr() {
+        return inspectDescr;
+    }
+
+    public String[] {
+        
+    }
 }
