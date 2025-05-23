54688a83acf5821384a34902df206a24ebee7c60
Unseile <lea.haeberli03@gmail.com>
2025-05-23 18:16:45 +0200

    Update ComLook.java

--- src/gameClasses/commands/ComLook.java
+++ src/gameClasses/commands/ComLook.java
@@ -14,26 +14,31 @@ public class ComLook extends Command {
     }
 
     private void displayLook(Location location) {
-        System.out.println("You see a " + location.getItemList().size() + " items");
         int i;
 
         if(location.getItemList().size() == 0 && location.getPuzzleList().size() == 0) {
             System.out.println("There are no items in this location.");
         } else {
+
+            System.out.println("You see a " + location.getItemList().size() + " items");
+
             for (i = 0; i <= location.getItemList().size(); i++) {
-                System.out.println("a " + location.getItemList().get(i).getLookDescr());
+                System.out.print("a " + location.getItemList().get(i).getLookDescr());
                 if (i < location.getItemList().size()) {
-                    System.out.println(", ");
+                    System.out.print(", ");
+                } else if (location.getPuzzleList().size() == 0) {
+                    System.out.print(". ");
                 } else {
-                    System.out.println(".");
+                    System.out.print(". ");
                 }
             }
 
             for (i = 0; i <= location.getPuzzleList().size(); i++) {
                 System.out.println("a " + location.getPuzzleList().get(i).getDescrPuzzle());
                 if (i < location.getItemList().size()) {
-                    System.out.println(", ");
-                    System.out.println(".");
+                    System.out.print(", ");
+                }else {
+                    System.out.print(".");
                 }
             }
         }
