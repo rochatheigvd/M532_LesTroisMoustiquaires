cf4b03f38fb37d6ae08dad41432226ec4f47a4d9
rochatheigvd <yannis.rochat@hes-so.ch>
2025-05-25 19:06:17 +0200

    debut use et take

--- src/gameClasses/commands/ComTake.java
+++ src/gameClasses/commands/ComTake.java
@@ -1,6 +1,7 @@
 package gameClasses.commands;
 
-import gameClasses.Command;
+import java.util.List;
+import gameClasses.*;
 
 public class ComTake extends Command {
     public ComTake(String verb, String description) {
@@ -9,6 +10,27 @@ public class ComTake extends Command {
 
     @Override
     public void execute(String argument) {
+        if (argument != null) {
+            Item item = itemFinder(argument);
+            if (item != null) {
+                getGame().getPlayer().getInventory().addItem(item);
+                getPlayerLocation().dropItem(item);
+                System.out.println(item.getName() + " is in your inventory.");
+            } else {
+                System.out.println("There is no object with this name here.");
+            }
+        } else {
+            System.out.println("There is not argument in your command.");
+        }
+    }
 
+    public Item itemFinder(String argument) {
+        List<Item> items = getPlayerLocation().getItemList();
+        for (Item item : items) {
+            if (item.getName().toLowerCase().equals(argument)) {
+                return item;
+            }
+        }
+        return null;
     }
 }
--- src/gameClasses/commands/ComUse.java
+++ src/gameClasses/commands/ComUse.java
@@ -1,14 +1,29 @@
 package gameClasses.commands;
 
+import java.util.List;
+
 import gameClasses.Command;
+import gameClasses.Item;
 
 public class ComUse extends Command {
-  public ComUse(String verb, String description) {
+    public ComUse(String verb, String description) {
         super(verb, description);
     }
 
-  @Override
+    @Override
     public void execute(String argument) {
+        if (argument != null) {
+            Item item = itemFinder(argument);
+            if (item != null) {
+                // code à implémenter ici.
+            }
+            System.out.println("There is no object with this name in your inventory.");
+        } else {
+            System.out.println("There is not argument in your command.");
+        }
+    }
 
+    public Item itemFinder(String argument) {
+        return getGame().getPlayer().getInventory().getItem(argument);
     }
 }
