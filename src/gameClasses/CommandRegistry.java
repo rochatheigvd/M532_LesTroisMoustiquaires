7d97f220bf4b0779b48fcc264aa2ce62eeb17211
rochatheigvd <yannis.rochat@hes-so.ch>
2025-05-27 17:52:32 +0200

    Update CommandRegistry.java

--- src/gameClasses/CommandRegistry.java
+++ src/gameClasses/CommandRegistry.java
@@ -2,7 +2,7 @@ package gameClasses;
 
 import java.util.ArrayList;
 
-public class CommandRegistry{
+public class CommandRegistry {
     private ArrayList<Command> commandList;
 
     public CommandRegistry() {
