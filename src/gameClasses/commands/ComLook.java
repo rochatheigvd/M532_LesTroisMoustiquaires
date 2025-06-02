b0983ede6c32e0a2b545af952c9c637001b76d9a
rochatheigvd <yannis.rochat@hes-so.ch>
2025-06-02 11:05:38 +0200

    Update ComLook.java

--- src/gameClasses/commands/ComLook.java
+++ src/gameClasses/commands/ComLook.java
@@ -15,36 +15,40 @@ public class ComLook extends Command {
 
     private void displayLook(Location location) {
         System.out.println(location.getName() + " description: " + location.getDescr());
-        System.out.print("You see " + location.getItemList().size() + " item");
-        if (location.getItemList().size() > 1) {
-            System.out.print("s");
-        } else if (location.getItemList().size() == 1){
-            System.out.print("");
-        }
-
-        System.out.print(" and " + location.getPuzzleList().size() + " puzzle");
-        if (location.getPuzzleList().size() > 1) {
+        if (location.getItemList().isEmpty() && location.getPuzzleList().isEmpty()) {
+            System.out.println("There's nothing visible here.");
+        } else {
+            System.out.print("You see " + location.getItemList().size() + " item");
+            if (location.getItemList().size() > 1) {
                 System.out.print("s");
-        } else if (location.getPuzzleList().size() == 1){
+            } else if (location.getItemList().size() == 1) {
                 System.out.print("");
-        }
-        System.out.println(".");
-        int i;
-        for (i = 0; i < location.getItemList().size(); i++) {
-            System.out.print("  - " +
-                    location.getItemList().get(i).getName() + ": " + location.getItemList().get(i).getLookDescr());
-            if (i < location.getItemList().size() - 1) {
-                System.out.print(", ");
-            } else {
-                System.out.println(".");
             }
-        }
-        for (i = 0; i < location.getPuzzleList().size(); i++) {
-            System.out.print("  - Puzzle " + (i + 1) + ": " + location.getPuzzleList().get(i).getDescrPuzzle());
-            if (i < location.getPuzzleList().size() - 1) {
-                System.out.print(", ");
-            } else {
-                System.out.println(".");
+
+            System.out.print(" and " + location.getPuzzleList().size() + " puzzle");
+            if (location.getPuzzleList().size() > 1) {
+                System.out.print("s");
+            } else if (location.getPuzzleList().size() == 1) {
+                System.out.print("");
+            }
+            System.out.println(".");
+            int i;
+            for (i = 0; i < location.getItemList().size(); i++) {
+                System.out.print("  - " +
+                        location.getItemList().get(i).getName() + ": " + location.getItemList().get(i).getLookDescr());
+                if (i < location.getItemList().size() - 1) {
+                    System.out.print(", ");
+                } else {
+                    System.out.println(".");
+                }
+            }
+            for (i = 0; i < location.getPuzzleList().size(); i++) {
+                System.out.print("  - Puzzle " + (i + 1) + ": " + location.getPuzzleList().get(i).getDescrPuzzle());
+                if (i < location.getPuzzleList().size() - 1) {
+                    System.out.print(", ");
+                } else {
+                    System.out.println(".");
+                }
             }
         }
     }
