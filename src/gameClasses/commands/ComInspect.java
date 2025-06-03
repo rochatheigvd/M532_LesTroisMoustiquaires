b9b1efa1af7490520ede7cbdc5b440d503f966c7
Unseile <lea.haeberli03@gmail.com>
2025-06-03 11:56:41 +0200

    Update ComInspect.java

--- src/gameClasses/commands/ComInspect.java
+++ src/gameClasses/commands/ComInspect.java
@@ -1,7 +1,10 @@
 package gameClasses.commands;
+<<<<<<< HEAD
 
 import java.util.List;
 
+=======
+>>>>>>> Léa
 import gameClasses.Command;
 import gameClasses.Item;
 
@@ -12,14 +15,26 @@ public class ComInspect extends Command {
     }
 
     @Override
-    public void execute(String argument) {
-        if (argument != null) {
+public void execute(String argument) {
+    if (argument != null) {
+        // Vérifie si l'argument est un nombre
+        try {
+            int index = Integer.parseInt(argument);
+            if (index >= 1 && index <= getGame().getPlayer().getInventory().getItemList().size()) {
+                Item selectedItem = getGame().getPlayer().getInventory().getItemList().get(index - 1);
+                displayItem(selectedItem);
+            } else {
+                System.out.println("Numéro d'item invalide.");
+            }
+        } catch (NumberFormatException e) {
+            // Si ce n'est pas un nombre, on cherche par nom
             Item i = getGame().getPlayer().getInventory().getItem(argument);
             if (i != null) {
                 displayItem(i);
             } else {
-                System.out.println("This item does not exist in your inventory.");
+                System.out.println("Cet item n'existe pas dans votre inventaire.");
             }
+<<<<<<< HEAD
         } else {
             List<Item> i = getGame().getPlayer().getInventory().getItemList();
             if (i.size() > 0) {
@@ -52,10 +67,18 @@ public class ComInspect extends Command {
             } else {
                 System.out.println("There is nothing in your inventory.");
             }
+=======
+        }
+    } else {
+        // Affiche l'inventaire numéroté
+        for (int j = 0; j < getGame().getPlayer().getInventory().getItemList().size(); j++) {
+            Item item = getGame().getPlayer().getInventory().getItemList().get(j);
+            System.out.println((j + 1) + ". " + item.getName());
+>>>>>>> Léa
         }
     }
-
-    private void displayItem(Item i) {
+}
+    private void displayItem(Item i){
         System.out.println("Inspecting " + i.getName());
         System.out.println("The item says " + '"' + i.inspect() + '"' + '.');
     }
